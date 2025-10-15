88aa1dc343ef74f0ea6a8ab83b2bc74796fa4bf7
Rudzios <skowix@gmail.com>
2025-10-15 21:37:40 +0200

    Add files via upload

---
+++ motivator.py
@@ -0,0 +1,4 @@
+name=input("Podaj swoje imie: ")
+subject_of_day=input("Podaj temat dnia który cię dziś interesuje: ")
+hours=input("Podaj ilość planowanych godzin: ")
+print(f"{name} dziś twoim celem jest {hours} godziny {subject_of_day}ego")
